714001efe7798619c56e84325c230936af088db7
yuanjing <yuanjingjy@163.com>
2019-01-06 19:52:43 +0800

    customized method

--- preprocessing.py
+++ preprocessing.py
@@ -36,43 +36,49 @@ class preprocessing(object):
         data['bmi_label'] = 0
         index_bmi = data[data['height'].isnull() | data['weight'].isnull()].index
         data['bmi_label'].loc[index_bmi] = 1
-
-        # # 提取所有的数值型特征的名称
-        # keys = list(data.keys())
-        # for key in ['admission_type', 'gender', 'vaso', 'vent', 'bmi_label']:
-        #     keys.remove(key)
-        #
-        # # 定义对数值型变量和分类型变量的处理方法
-        # numeric_features = keys  # impute the missing value with median for the numeric eigen
-        # numeric_transformer = Pipeline(steps=[
-        #     ('imputer', SimpleImputer(strategy='mean')),
-        #     ('scaler', QuantileTransformer(random_state=0, output_distribution='uniform'))
-        # ])
-        #
-        # categorical_features = ['admission_type', 'gender', 'vaso', 'vent',
-        #                         'bmi_label']  # impute the missing value with 'missing' and Encode them for the categorical eigens
-        # categorical_transformer = Pipeline(steps=[
-        #     ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
-        #     ('onehot', OneHotEncoder(handle_unknown='ignore'))
-        # ])
-        #
-        # # 预处理模块，ColumnTransformer函数分别处理数值型特征和分类型特征
-        # preprocessor_imp = ColumnTransformer(
-        #     # preprocessing numeric and categorical eigen with different methods respectively
-        #     transformers=[
-        #         ('num', numeric_transformer, numeric_features),
-        #         ('cat', categorical_transformer, categorical_features)
-        #     ]
-        # )
-        #
-        # # preprocessor_imp.fit(data)
-        # data_processed = preprocessor_imp.transform(data)
-
         data_processed = data
 
         return data_processed
 
     def fit(self,X,y=None):
+        return self
+
+class preimp(object):
+    def transform(self,data):
+        # 提取所有的数值型特征的名称
+        keys = list(data.keys())
+        for key in ['admission_type', 'gender', 'vaso', 'vent', 'bmi_label']:
+            keys.remove(key)
+
+        # 定义对数值型变量和分类型变量的处理方法
+        numeric_features = keys  # impute the missing value with median for the numeric eigen
+        numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='mean')),
+            ('scaler', QuantileTransformer(random_state=0, output_distribution='uniform'))
+        ])
+
+        categorical_features = ['admission_type', 'gender', 'vaso', 'vent',
+                                'bmi_label']  # impute the missing value with 'missing' and Encode them for the categorical eigens
+        categorical_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        # 预处理模块，ColumnTransformer函数分别处理数值型特征和分类型特征
+        preprocessor_imp = ColumnTransformer(
+            # preprocessing numeric and categorical eigen with different methods respectively
+            transformers=[
+                ('num', numeric_transformer, numeric_features),
+                ('cat', categorical_transformer, categorical_features)
+            ]
+        )
+
+        preprocessor_imp.fit(data)
+        data_processed = preprocessor_imp.transform(data)
+
+        return data_processed
+
+    def fit(self, X, y=None):
         return self
 
 def main():
@@ -83,13 +89,14 @@ def main():
     labelmat = data['label']
     datamat = data.drop(['label'], axis=1)
 
-
-    pipeline = Pipeline([('preprocess', preprocessing())])
+    pipeline = Pipeline([('preprocess', preprocessing()),
+                         ('test',preimp())
+                        ])
+    pipeline.fit(datamat)
     test = pipeline.transform(datamat)
     return test
 
 if __name__ == '__main__':
     data = main()
     print(data)
-
 
